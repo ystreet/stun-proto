80a62766ce428870b5963b2f8c032b93ac8741c8
Matthew Waters <matthew@centricular.com>
2024-06-20 18:33:56 +1000

    agent/test: add test for response without credentials

--- stun-proto/src/agent.rs
+++ stun-proto/src/agent.rs
@@ -944,6 +944,40 @@ pub(crate) mod tests {
     }
 
     #[test]
+    fn response_without_credentials() {
+        init();
+        let local_addr = "10.0.0.1:12345".parse().unwrap();
+        let remote_addr = "10.0.0.2:3478".parse().unwrap();
+
+        let mut agent = StunAgent::builder(TransportType::Udp, local_addr).build();
+        let local_credentials = ShortTermCredentials::new(String::from("local_password"));
+        let remote_credentials = ShortTermCredentials::new(String::from("remote_password"));
+        agent.set_local_credentials(local_credentials.into());
+        agent.set_remote_credentials(remote_credentials.clone().into());
+
+        let mut msg = Message::new_request(BINDING);
+        msg.add_message_integrity(&remote_credentials.into(), IntegrityAlgorithm::Sha1)
+            .unwrap();
+        let transmit = agent.send(msg, remote_addr).unwrap();
+
+        let request = Message::from_bytes(&transmit.data).unwrap();
+
+        let mut response = Message::new_success(&request);
+        response
+            .add_attribute(XorMappedAddress::new(
+                transmit.from,
+                request.transaction_id(),
+            ))
+            .unwrap();
+
+        let data = response.to_bytes();
+        let to = transmit.to;
+        let reply = agent.handle_incoming_data(&data, to).unwrap();
+        // reply is ignored as it does not have credentials
+        assert!(reply.is_empty());
+    }
+
+    #[test]
     fn tcp_request() {
         init();
         let local_addr = "127.0.0.1:2000".parse().unwrap();
