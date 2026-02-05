649f0f07e229a2e91e78cb4c542aa7d0bc839c51
Tim-Philipp Müller <tim@centricular.com>
2026-02-05 13:40:44 +0000

    Add SPDX-License-Identifier

--- stun-types/benches/message_write.rs
+++ stun-types/benches/message_write.rs
@@ -5,6 +5,8 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use stun_types::attribute::*;
